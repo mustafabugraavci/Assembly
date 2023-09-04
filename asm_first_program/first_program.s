9ce52027e42043db7869348b7aa23c6448cef66d
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2023-09-04 16:34:23 +0000

    First Program

---
+++ asm_first_program/first_program.s
@@ -0,0 +1,12 @@
+.global _start
+_start:
+	mov r0,#30
+	
+	mov r7, #1
+	swi 0
+
+;This ARM assembly code sets the r0 register to 30, 
+;places 1 in the r7 register, 
+;and triggers a software interrupt with code 0, 
+;typically used for making system calls in ARM-based operating systems.
+
