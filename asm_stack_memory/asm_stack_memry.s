6b90215eb53917ee8c44da5157f53d922779995b
Mustafa Buğra Avcı <mbugraavci38@gmail.com>
2023-09-13 10:31:22 +0000

    Stack Memory

--- asm_stack_memory/asm_stack_memry.s
+++ asm_stack_memory/asm_stack_memry.s
@@ -1,13 +1,17 @@
 .global _start
 _start:
-    mov r0, #2       // Load the value 2 into register R0
-    mov r1, #4       // Load the value 4 into register R1
-    cmp r0, r1       // Compare the values in R0 and R1
+	
+	mov r0, #1
+	mov r1, #3
+	push {r0, r1}
+	bl get_value
+	pop {r0, r1}
+	b end
+	
+get_value:
+	mov r0, #5
+	mov r1, #2
+	add r2, r1, r0
+	bx lr
 
-    addlt r2, #1     // If R0 is less than R1 (LT: Less Than), add 1 to R2
-
-    mov r0, #6       // Load the value 6 into register R0
-    mov r1, #4       // Load the value 4 into register R1
-    cmp r0, r1       // Compare the values in R0 and R1
-
-    movge r3, #1     // If R0 is greater than or equal to R1 (GE: Greater Than or Equal), set R3 to 1
+end:
