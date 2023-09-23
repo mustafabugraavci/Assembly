6736c63a9ac10d00b78618d6d998e11a2881d3fb
Mustafa Buğra Avcı <mustafabugraavci38@gmail.com>
2023-09-23 15:18:10 +0300

    ASM with Makefile

--- asm_with_make/src/main_as.s
+++ asm_with_make/src/main_as.s
@@ -1,54 +1,34 @@
-.global matrix_multiply
-
-matrix_multiply:
+.global calculate_fibonacci
+calculate_fibonacci:
     // Input:
-    // r0: Pointer to matrix A
-    // r1: Pointer to matrix B
-    // r2: Pointer to the result matrix C
-    // r3: Matrix size (number of rows/columns)
+    // r0: n (the Fibonacci number to compute)
     
-    push {r4, r5, r6, r7}
+    // Base cases: F(0) = 0, F(1) = 1
+    cmp r0, #1
+    itt ls
+    movls r0, #1
+    movls pc, lr
     
-    mov r4, r0      // Copy the address of matrix A to r4
-    mov r5, r1      // Copy the address of matrix B to r5
-    mov r6, r2      // Copy the address of matrix C to r6
+    // Initialize variables
+    mov r1, #0   // F(0)
+    mov r2, #1   // F(1)
+    mov r3, #2   // Current index (starts from 2)
+
+calculate_loop:
+    // Compute F(n) = F(n-1) + F(n-2)
+    add r4, r1, r2
+    mov r1, r2
+    mov r2, r4
     
-    // Clear the result matrix C
-    ldr r7, =0
-    mov r8, r3
-    muls r8, r8, #4
-    clear_loop:
-        str r7, [r6], #4
-        subs r8, r8, #4
-        bne clear_loop
+    // Increment current index
+    add r3, r3, #1
     
-    // Perform matrix multiplication
-    mul_loop_outer:
-        mov r9, r0      // Reload the address of matrix A to r9
-        mul_loop_inner:
-            ldr r10, [r5], #4
-            ldr r11, [r9]
-            mul r10, r10, r11
-            ldr r11, [r6]
-            add r11, r11, r10
-            str r11, [r6]
-            add r9, r9, #4
-            subs r8, r8, #1
-            bne mul_loop_inner
-        
-        // Move to the next row of matrix A
-        add r0, r0, r3, lsl #2
-        
-        // Move to the next row of matrix C
-        add r6, r6, r3, lsl #2
-        
-        // Reset the column pointer of matrix B
-        mov r5, r1
-        
-        // Decrease the row counter
-        subs r7, r7, #1
-        bne mul_loop_outer
+    // Check if we've reached n
+    cmp r3, r0
+    bne calculate_loop
     
-    pop {r4, r5, r6, r7}
+    // Result is in r2, store it in the GRV
+    str r2, [r0]
+    
     bx lr
-	
+    
